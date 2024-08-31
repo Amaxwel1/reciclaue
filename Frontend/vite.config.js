648777e7da89b1b246cd99fbc1b1a49877058563
Amaxwel1 <alexmaxwellbg@gmail.com>
2024-08-31 16:00:46 -0300

    init project

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-dotenv.config();
-
-console.log("VITE_API_URL:", process.env.VITE_API_URL); // Verifique se a variável está carregada
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "import.meta.env": process.env,
-  },
-  server: {
-    port: 5173, // Porta para o servidor de desenvolvimento Vite
-  },
-  build: {
-    outDir: "dist", // Diretório de saída para os arquivos de build
-  },
-});
+})
